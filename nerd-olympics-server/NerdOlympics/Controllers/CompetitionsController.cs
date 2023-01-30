87289b79483e05b40e8104e27bf65dbc002d0d7e
Miguel Verdelho <miguel@verdelho.net>
2023-01-30 22:27:38 +0000

    dotnet project first commit

---
+++ nerd-olympics-server/NerdOlympics/Controllers/CompetitionsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace NerdOlympics.Controllers;
+
+[ApiController]
+[Route("competitions")]
+public class CompetitionsController : Controller
+{
+    private readonly ILogger<CompetitionsController> _logger;
+
+    public CompetitionsController(ILogger<CompetitionsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("echo")]
+    public string Echo(string input)
+    {
+        return input;
+    }
+}
