8e84f0a7f5b1e232b00ca0a6180f8c52e3b580ac
Miguel Verdelho <miguel@verdelho.net>
2023-01-30 23:13:48 +0000

    data project creation

---
+++ nerd-olympics-server/Data/Models/Data/Competition.cs
@@ -0,0 +1,8 @@
+namespace NerdOlympics.Models;
+
+public class Competition {
+    int CompetitionId {get;set;}
+    string? Name {get;set;}
+    string? Description {get;set;}
+    
+}
---
+++ nerd-olympics-server/Data/Models/Data/User.cs
@@ -0,0 +1,8 @@
+namespace NerdOlympics.Models;
+
+public class User{
+    int UserId {get;set;}
+    string? Name {get;set;}
+    string? EmailAddress {get;set;}
+    string? Password {get;set;}
+}
